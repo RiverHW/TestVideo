438172f2c14cc9de13c6360b1e1f76a80e1e5d95
RiverHW <1048851186@qq.com>
2024-08-29 16:04:13 +0800

    Initial Commit

---
+++ TestVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestVideo
+//
+//  Created by edy on 2024/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
